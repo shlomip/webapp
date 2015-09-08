c7f0abb1e06224d0496628861db50b2412ef2e8f
shlomip <shlomiperl666@gmail.com>
2015-09-08 06:42:14 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -20,28 +20,35 @@ var tabArray = ["#quick-reports", "#my-folders", "#my-team-folders", "#public-fo
 
 $(".tab").hide();
 
-$("#quick-reports").show();
+select_tab();
 
 $("#tab-quick-reports").click(function(){
+	$(".tab").hide();
+	$(".tab-head").css("background-color","e6e6e6");
 	$("#quick-reports").show();
 	$("#tab-quick-reports").css("background-color","#e6e6e6");
 	localStorage.setItem("last-tab", "#quick-reports");
 	hide-rest("#quick-reports");
-
 }); 
 $("#tab-my-folders").click(function(){
+	$(".tab").hide();
+	$(".tab-head").css("background-color","#525252");
 	$("#my-folders").show();
 	$("#tab-my-folders").css("background-color","#e6e6e6");
 	localStorage.setItem("last-tab", "#my-folders");
 	hide-rest("#my-folders");
 }); 
 $("#tab-my-team-folders").click(function(){
+	$(".tab").hide();
+	$(".tab-head").css("background-color","#525252");
 	$("#my-team-folders").show();
 	$("#tab-my-team-folders").css("background-color","#e6e6e6");
 	localStorage.setItem("last-tab", "#my-team-folders");
 	hide-rest("#my-team-folders");
 }); 
 $("#tab-public-folders").click(function(){
+	$(".tab").hide();
+	$(".tab-head").css("background-color","#525252");
 	$("#public-folders").show();
 	$("#tab-public-folders").css("background-color","#e6e6e6");
 	localStorage.setItem("last-tab", "#public-folders");
@@ -49,7 +56,22 @@ $("#tab-public-folders").click(function(){
 }); 
 
 $("#settings").click(function(){
-	$(".quick-reports-wrapper").toggle();
+	$(".reports-wrapper").toggle();
 
 });
+
+
+function select_tab () {
+	// body...
+if(localStorage.getItem("last-tab")=="#quick-reports")
+	$("#quick-reports").show();
+else if(localStorage.getItem("last-tab")=="#my-folders")
+	$("#my-folders").show();
+else if(localStorage.getItem("last-tab")=="#my-team-folders")
+	$("#my-team-folders").show();
+else if(localStorage.getItem("last-tab")=="#public-folders")
+	$("#public-folders").show();
+else 
+	$("#quick-reports").show(); 
+}
 //}
