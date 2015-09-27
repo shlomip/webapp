1b5c6ccbb615b42b734438f9005a6d79a09c655a
shlomip <shlomiperl666@gmail.com>
2015-09-27 12:22:29 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
 
-const TAB_LIST = ['#quick-reports', '#my-folders', '#my-team-folders', '#public-folders'];
-const QUICK_REPORTS = 0;
-const MY_FOLDERS = 1;
-const MY_TEAM_FOLDERS = 2;
-const PUBLIC_FOLDERS = 3;
-const NO_OPEN_TAB = 4;
+const TAB_LIST = ['#quick-reports', '#my-folders', '#my-team-folders', '#public-folders', 'no-open-tabs'];
+//var TabNumbers = {};
+//var TabNumbers = {QUICK_REPORTS:0, MY_FOLDERS:1, MY_TEAM_FOLDERS:2, PUBLIC_FOLDERS:3, NO_OPEN_TAB:4};
+
 var Tabs = $(".tab-headers li");
-var CurrentTab = NO_OPEN_TAB; 
+var CurrentTab = "no-open-tabs"; 
 const SITE_TABS = [ 'FirstTab', 'SecondTab', 'ThirdTab'];
+
 
 function load_settings(){
 
@@ -24,10 +23,24 @@ function load_settings(){
 		UpdateStorage( CurrentTab, "LastTab" );
 	});
 	
-
 	//location.hash =  Storage.LastTab;
 	CurrentTab = Storage.LastTab;
 	$(CurrentTab).show();
+
+	var selected = "";
+	switch(CurrentTab){
+		case '#quick-reports': selected = "#tab-quick-reports"; break;
+		case '#my-folders': selected = "#tab-my-folders"; break;
+		case '#public-folders': selected = "#tab-public-folders";break;
+		case '#my-team-folders':selected = "#tab-my-team-folders";break;
+	}
+
+	$(selected).css("background-color", " rgb(230, 230, 230)");
+	console.log(selected);
+
+	TabNum = Storage.TabNum;
+	
+
 	UpdateTabSites("#quick-reports");
 	UpdateTabSites("#my-team-folders");
 	//console.log("current: "+ CurrentTab);
@@ -57,7 +70,7 @@ function load_settings(){
 	// There is storage in the first tab
 	if(Storage["FirstTab"] != undefined)
 	{
-		SitesList = $("CurrentTab .report");
+		SitesList = $("#quick-reports .report");
 		//SitesList = Storage.
 		//update_dropdown(url, name);
 		for (var i = 0; i < Storage["FirstTab"].length; i++) 
@@ -73,7 +86,7 @@ function load_settings(){
 	// There is storage in the third tab
 	if(Storage["ThirdTab"] != undefined)
 	{
-		SitesList = $("#SecondList fieldset");
+		SitesList = $("#my-team-folders .report");
 		for (var i = 0; i < Storage["ThirdTab"].length; i++) 
 		{
 			var SiteValue = $(SitesList[i]).find("input");
@@ -83,22 +96,6 @@ function load_settings(){
 		if(Storage["ThirdTab"].length > 0)
 			UpdateTabSites(MY_TEAM_FOLDERS);
 	}
-	/*$.ajax({
-        type:'GET',
-        url: 'C:\Users\shlomi\Documents\GitHub\webapp\data\config.json',
-        contentType: 'plain/text; charset=UTF-8',
-        dataType: 'json',
-        success: function(data){
-
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-
-        },
-        complete: function(jqXHR, textStatus){
-
-            initConfig($.parseJSON(jqXHR.responseText));
-        }
-    });*/
 
 	return;
 }
@@ -156,89 +153,90 @@ function UpdateStorage( Value, TabName ){
 
 function SaveSites( SitesList ){ 
 	var ValidEntry = [];
-	var EntryFlag = false;
+	var Flag = false;
 	//var SitesList = $(SitesForm).find("report"); 
 	//console.log(SitesList.length);
 
-	for (var i = 0; i < SitesList.length; i++) {
+	for (var i = 0; i < SitesList.length-1 ; i++) {
 		var Entry = $(SitesList[i]).find("input");
 		//console.log(Enrty);
 
-		SiteEntry = $(Entry[0]);
+		NameEntry = $(Entry[0]);
 		URLEntry = $(Entry[1]);
 		
 		Name = Entry[0];
 		URL = Entry[1];
-
-		if( SiteEntry.val() != "" && URLEntry.val() != "" ) 
+		//if(URL.value!=null && Name.value!=null){
+		if( NameEntry.val() != "" && URLEntry.val() != "" ) 
 		{
-			EntryFlag = true;
+			Flag = true;
+			console.log(i);
 			// add http:// if doesn't exist
 			if( !URL.value.match("^http") )
 			{
-				var SiteName = 'http://' + URL.value;
-			}
-			else
-			{
-				var SiteName = URL.value;
+				var urlVal = 'http://' + URL.value;
+				URL.value = urlVal;
+				console.log(urlVal);
 			}
 			
 			// check url validity
 			var url_validate = /^(http:\/\/www\.|https:\/\/www\.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
-			if(!url_validate.test(SiteName))
+			if(!url_validate.test(URL.value))
 			{
 				alert('invalid URL');
-				URLEntry.addClass('red-border');
+				URLEntry.css("border-color","red");
 				return;
 			}
 
 			ValidEntry.push({'site':Name.value, 'url':URL.value});
 			//console.log(Name.value, SiteName);
-			SiteEntry.removeClass('red-border');
-			URLEntry.removeClass('red-border');
+			NameEntry.css("border-color","inherit");
+			URLEntry.css("border-color","inherit");
+
 			var option = document.createElement("option");
 		    option.text = Name.value;
 			option.value = URL.value;
+
 			if(CurrentTab=="#quick-reports"){
 				//$(CurrentTab).find(".choose-iframe-select").add(option);
 				document.getElementById("choose-iframe").add(option);
 			}
-			else {//if(CurrentTab=="#my-team-folders"){
+			else if(CurrentTab=="#my-team-folders"){
 				document.getElementById("team-choose-iframe").add(option);
-			}
-					
+			}	
 			// one of the entries is empty
-			if( SiteEntry.val() != "" && URLEntry.val() == "" ) 
-			{
-				SiteEntry.removeClass('red-border');
-				URLEntry.addClass('red-border');
-				URL.focus();
-				return;
-			}
-			if( SiteEntry.val() == "" && URLEntry.val() != "" ) 
-			{
-				URLEntry.removeClass('red-border');
-				SiteEntry.addClass('red-border');
-				Name.focus();
-				return;
-			}						
+		if( NameEntry.val() != "" && URLEntry.val() == "" ) 
+		{
+			NameEntry.css("border-color","red");
+			URLEntry.css("border-color","red");
+			URL.focus();
+			return;
 		}
+		if( NameEntry.val() == "" && URLEntry.val() != "" ) 
+		{
+			URLEntry.css("border-color","inherit");
+			NameEntry.css("border-color","inherit");
+			Name.focus();
+			return;
+		}					
+		}
+	//}
+		
 	}
 
-	UpdateStorage( ValidEntry, SITE_TABS[CurrentTab] );			
-	if(EntryFlag) 
+	//UpdateStorage( ValidEntry, SITE_TABS[CurrentTab] );
+	UpdateStorage( ValidEntry, TAB_LIST[CurrentTab] );	
+	if(Flag) 
 	{
 		UpdateTabSites(CurrentTab); //CurrentTab == #xxxxx
-		var TabSelect = $(CurrentTab);	
-		TabSelect.find("select").show();		
+		$(CurrentTab).find("select").show();		
 		//console.log(TabSelect);
 	}
 	else
 	{ 
-		var TabSelect = $(CurrentTab);
-		TabSelect.find("iframe").hide(); 
+		$(CurrentTab).find("iframe").hide(); 
 		//TabSelect.find("select").hide();
-		TabSelect.find(".expend").hide(); 
+		$(CurrentTab).find(".expend").hide(); 
 	}
 }
 
@@ -249,15 +247,16 @@ function UpdateTabSites(Tab){
 	var SiteValue = Storage[TAB_LIST[Tab]];	
 	var SelectFlag = true; 
 
-	//console.log(TabSelect,SiteValue);
+	//console.log(SiteValue);
 
 	if( TabSelect.find("select").length > 0 )
 	{
-		Value = TabSelect.find("select")[0];
+		Value = $(Tab).find("select")[0];
+		//console.log(Value);
 	}
 	else
 	{
-		Value = document.createElement("select"); 
+		//Value = document.createElement("select"); 
 		SelectFlag = false;
 	}
 
@@ -266,14 +265,28 @@ function UpdateTabSites(Tab){
 		Value.remove(i);
 	}
 
-	for (var i = 0; i < SiteValue.length; i++)
+	var SitesList = $(Tab).find("form"); 
+
+	for (var i = 0; i < SiteValue.length ; i++)
 	{
-		var Site = document.createElement("option");
-		Site.value = SiteValue[i].url;
-		Site.innerHTML = SiteValue[i].site;
-		Value.add(Site);
-		//console.log(SiteValue[i].url, SiteValue[i].site);
-	};
+		var option = document.createElement("option");
+		//console.log(SiteValue[i]);
+		option.value = SiteValue[i].url;
+		option.innerHTML = SiteValue[i].site;
+		Value.add(option);
+
+	if(i < SitesList.length){
+		var Entry = $(SitesList[i]).find("input");
+
+		
+			var Name = Entry[0];
+			var URL = Entry[1];
+
+			//console.log(URL.value);
+			URL.value = SiteValue[i].url;
+			Name.value = SiteValue[i].site;
+		}
+	}
 
 	if(!SelectFlag)
 	{
@@ -295,9 +308,12 @@ function UpdateTabSites(Tab){
 		TabIframe = document.createElement("iframe");
 		TabSelect.find(".iframe-build").append(TabIframe);			
 	}
-
+	if(SiteValue[0]!=null)
 	TabIframe.src = SiteValue[0].url;
+	//console.log(TabIframe.src);
 	TabSelect.find(".expand").show();
+	$(CurrentTab).find(TabIframe).show();
+	//$(CurrentTab).find("iframe-build").show();
 	//TabSelect.find(".list-form").hide(); 
 }
 
@@ -315,11 +331,11 @@ function hideRest(){
 load_settings();	
 
 $(window).on('hashchange', function(){
-	var TabNumber = TAB_LIST.indexOf(location.hash.slice(1));
+	var Tab = TAB_LIST.indexOf(location.hash.slice(1));
 	var TabSelect = $(CurrentTab);
-	if( CurrentTab != TabNumber){
+	if( CurrentTab != Tab){
 		TabSelect = $(CurrentTab);
-		if( TabNumber == QUICK_REPORTS || TabNumber == MY_TEAM_FOLDERS ){
+		if( Tab == "#quick-reports" || Tab == "#my-team-folders" ){
 			TabSelect.find(".settings").focus();
 		}
 		else{
@@ -340,19 +356,79 @@ function tabSelect (tab) {
 	location.hash = tab;
 	var Storage = GetStorage();
 	Storage.LastTab = CurrentTab;
+	//Storage.TabNum = TabNum;
 	localStorage.setItem("webapp", JSON.stringify(Storage));
 
+	return false;
+
+}
+
+function Search(Input) 
+{
+	var flag = false;
+	// search in the first tab
+	if( SearchTab(Input, "#quick-reports") != -1 ){
+		flag = true;
+		var SiteNumber = SearchTab(Input, "#quick-reports");
+		var Tab = "#quick-reports";
+	}
+	// search in the third tab
+	else if( SearchTab(Input, "#my-team-folders") != -1 ){
+		flag = true;
+		var SiteNumber = SearchTab(Input, "#my-team-folders");
+		var Tab = "#my-team-folders";
+	}
+	// input was found 
+	if(flag) {
+		location.hash = TAB_LIST[Tab]; 
+		//var TabSelect = $('#' + (TAB_LIST[Tab]));
+		var TabSelect = $(TAB_LIST[Tab]);
+		TabSelect.find('select').prop('selectedIndex', SiteNumber); 
+		TabSelect.find('select').trigger('change');
+		$('.notifications').hide();
+		TabSelect.find("select").focus();
+	}
+	else{
+		$('.notifications').empty();
+		$('.notifications').append('\'' + Input + '\'' + ' does not exist.')
+		$('.notifications').show();
+	}
+}
+
+function SearchTab(Input, Tab) {
+
+	var Storage = GetStorage();
+	var Sites = [];
+	
+	if(Storage[Tab] != undefined){
+		Array = Storage[Tab];
+		for( i = 0; i < Array.length; i++ ){
+			if( Array[i]['site'] != undefined )	{
+				Sites.push(Array[i]['site'])
+			}
+		}
+	}
+	for(i=0; i < Sites.length; i++){	
+		if(Sites[i].indexOf(Input) != -1) {
+			return i;
+		}
+	}
+	return -1;
 }
 
 $("#tab-quick-reports").click(function(e){
 	e.preventDefault();
 	tabSelect("quick-reports");
+	//location.preventDefault();
+	//location.hash = "#quick-reports";
 	return false;
 }); 
 
 $("#tab-my-folders").click(function(e){
 	e.preventDefault();
 	tabSelect("my-folders");
+	//location.preventDefault();
+	//location.hash = "#my-folders";
 	return false;
 }); 
 
@@ -383,8 +459,13 @@ $(".expend").click(function(e){
 
 $(".save").click(function(){
 	var SelectedTab = $(CurrentTab);
-	console.log(CurrentTab);
+	//console.log(CurrentTab);
 	var SitesList = $(CurrentTab).find("form"); 
+	for(var i=0; i < SitesList.length; i++){
+		var Entry = $(SitesList[i]).find("input");
+		if(Entry[0]=="" || Entry[1]=="")
+			break;
+	}
 	SaveSites(SitesList);
 	//console.log(SitesList.length);
 	return false;
@@ -394,6 +475,7 @@ $(".cancel").click(function() {
 		var TabSelect = $('#' + (TAB_LIST[CurrentTab]));
 		TabSelect.find(".list-form").slideUp();
 		TabSelect.find(".settings").focus();
+		return false;
 });
 
 $("#choose-iframe").change(function(){
@@ -402,7 +484,33 @@ $("#choose-iframe").change(function(){
 	$("option:selected").click($("#quick-reports-iframe").attr("src", $("option:selected").attr("value")));
 	$("#quick-reports-iframe").show();
 	$(".reports-wrapper").hide();
+	return false;
 	
+});
+
+$(".search-box").submit(function(e) {
+	e.preventDefault(); 
+	var input = $(this).find('input').val();
+	Search(input);
+	return false;
+});
+
+// keyboard cancel
+$(".tab form input").keydown(function(e) {
+ 	if (e.keyCode == 27) 
+	{
+		var TabSelect = $('#' + (TAB_LIST[CurrentTab]));
+    	TabSelect.find(".cancel").trigger("click");
+	}
+});
+
+// keyboard save
+$(".tab form input").keydown(function(e) {
+ 	if (e.keyCode == 13) 
+	{
+		var TabSelect = $('#' + (TAB_LIST[CurrentTab]));
+    	TabSelect.find(".save").trigger("click");
+	}
 });
 
 })
